4a9ac2dffc111d2e0326ddc3c420b9befdac0760
Hamza Zoumhani <hamza.zoumhani@okstate.edu>
2019-02-16 00:21:03 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int h;
+int w;
+int age;
+
+void getHeight(){
+	cout << "Please enter your Height in inches: " ;
+	cin >> h;
+}
+
+void getWeight() {
+	cout << "Please enter your Weight in pounds: " ;
+	cin >> w;
+}
+
+void getAge(){
+	cout << "Please enter your Age in years: " ;
+	cin >> age;
+}
+
+void hatsize(){
+	double hat = (w/h)*2.9;
+	cout << "Your hat size is: " << hat << endl;
+}
+
+int main(){
+	getHeight();
+	getWeight();
+	getAge();
+	
+	hatsize();
+	
+	
+	
+	return 0;
+}
